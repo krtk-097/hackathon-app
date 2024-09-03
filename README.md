047620f86862225a90da4ab28fa824a0fc9fc8d0
Kartik <frankyz2190@gmail.com>
2024-09-04 01:17:57 +0530

    completed

---
+++ README.md
@@ -0,0 +1,3 @@
+#Hackathon-app
+
+Deployed link:
---
+++ src/utils/hackathonEvents.js
@@ -0,0 +1,44 @@
+import image4 from "../assets/cardimage/Group 1000002466.png";
+import image3 from "../assets/cardimage/Group 1000002767.png";
+import image2 from "../assets/cardimage/Group 1000002772.png";
+import image1 from "../assets/cardimage/Group 1000002773.png";
+
+export const hackathonEventsList = [
+  {
+    id: "1",
+    eventName: "Travel Insurance Claim Prediction",
+    startDate: "2024-09-04T10:55",
+    endDate: "2024-09-10T10:55",
+    eventDescription:
+      "This is Travel Insurance Claim prediction hackathon event",
+    eventImage: image1,
+    eventLevel: "Easy",
+  },
+  {
+    id: "2",
+    eventName: "Data Sprint 72 - Butterfly Identification",
+    startDate: "2024-09-21T11:30",
+    endDate: "2024-09-25T13:30",
+    eventDescription: "This is Butterfly identification hackathon event",
+    eventImage: image2,
+    eventLevel: "Medium",
+  },
+  {
+    id: "3",
+    eventName: "Data Science Bootcamp - Graded Datathon",
+    startDate: "2022-10-12T11:30",
+    endDate: "2022-10-25T13:30",
+    eventDescription: "This is Data Science Bootcamp hackathon event",
+    eventImage: image3,
+    eventLevel: "Hard",
+  },
+  {
+    id: "4",
+    eventName: "Data Sprint 70 - Airline",
+    startDate: "2024-09-04T07:30",
+    endDate: "2024-09-06T19:30",
+    eventDescription: "This is Data Sprint 70 - Airline hackathon event",
+    eventImage: image4,
+    eventLevel: "Easy",
+  },
+];
